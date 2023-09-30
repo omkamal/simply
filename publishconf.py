dc83a1484fecaaceb2c8ba7a9278aa4ccc3e4aa4
Omar Hosney <omar@tribal.credit>
2023-09-29 22:59:55 -0700

    updates

--- publishconf.py
+++ publishconf.py
@@ -7,8 +7,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://omkamal.github.io'
-RELATIVE_URLS = True
+SITEURL = ''
+RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
