28de6ff30ac41b120f11eccbc9e89d0b3833725a
Omar Kamal <omkamal@gmail.com>
2023-10-04 22:23:55 -0700

    Default Theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,7 +16,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 #DISQUS_SITENAME = 'http-omkamal-github-io-simply'
 #PELICAN_COMMENT_SYSTEM = True
-THEME = "./pelican-themes/simplify-theme"
+#THEME = "./pelican-themes/simplify-theme"
 
 
 PLUGIN_PATHS = ['pelican-plugins']
