c04a6f4fae8d4197fb8d089b764b42a3c6f563d6
Omar Kamal <omkamal@gmail.com>
2023-10-04 21:49:26 -0700

    Chaging Theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,6 +16,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 #DISQUS_SITENAME = 'http-omkamal-github-io-simply'
 #PELICAN_COMMENT_SYSTEM = True
+THEME = "./pelican-themes/gum"
 
 
 PLUGIN_PATHS = ['pelican-plugins']
