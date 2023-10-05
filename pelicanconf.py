b8665a5ac3457c3b33ce42f9ab8080992cab9af2
Omar Kamal <omkamal@gmail.com>
2023-10-04 22:01:24 -0700

    Updating Theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,7 +16,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 #DISQUS_SITENAME = 'http-omkamal-github-io-simply'
 #PELICAN_COMMENT_SYSTEM = True
-THEME = "./pelican-themes/bootstrap"
+THEME = "./pelican-themes/foundation-default-colours"
 
 
 PLUGIN_PATHS = ['pelican-plugins']
